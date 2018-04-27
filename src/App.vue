f2d9d698a57fa13a99c693721120ef9f2bb13fd7
Qian Zhenlei <zhenlei.qian@linecorp.com>
2018-04-27 22:25:54 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <el-row class="container" style="height: 100%">
+  <div>
     <v-header :user="user"></v-header>
-    <el-col :span="24" class="main">
-      <el-row>
-        <el-menu :default-active="$route.path" class="mar-l el-menu-vertical-demo el-col el-col-3" light router>
-          <template v-for="(item,index) in $router.options.routes[0].children" v-if="!item.hidden">
-            <el-menu-item :index="item.path" ><i class="fa" :class="item.class"></i>{{item.name}}</el-menu-item>
-          </template>
-        </el-menu>
-        <section class="contentCon">
-          <el-col :span="21" :offset="3" class="content-wrapper">
-            <transition>
-              <router-view></router-view>
-            </transition>
-          </el-col>
-        </section>
-      </el-row>
-    </el-col>
-  </el-row>
+    <el-menu :default-active="$route.path" router>
+      <el-submenu v-for="(first, index) in $router.options.routes[0].children" :key="index" :index="first.path">
+        <template slot="title">
+          <span>{{first.name}}</span>
+        </template>                
+        <el-menu-item-group v-for="item in first.children" v-if="!item.hidden" :key="item.path">
+          <el-menu-item :index="'/' + first.path + '/' +item.path"><i class="fa" :class="item.class"></i>{{item.name}}</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+    <section>
+        <transition>
+          <router-view></router-view>
+        </transition>
+    </section>
+  </div>
 </template>
 <script>
   import header from './views/header/header.vue';
@@ -29,6 +28,7 @@
       };
     },
     created () {
+      console.log(this.$route, this.$route.path.split('/')[1]);
       this.$http.get('/api/user').then((response) => {
         response = response.data;
         if (response.code === ERR_OK) {
@@ -47,45 +47,6 @@
   };
 </script>
 <style>
-
-
-  /* fa图标右侧需要流出空白 elementUI图标已自带样式 */
-  i.fa {
-    vertical-align: baseline;
-    margin-right: 10px;
-  }
-  body {
-    font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
-    background-color: #F2F2F2;
-  }
-  .menu,
-  .el-menu {
-    height: 100%;
-    background-color: #E6E6E6;
-    position: fixed;
-    float: left;
-  }
-
-  .container {
-    padding-top: 80px;
-    height: 100%;
-  }
-
-  .container .main {
-    padding: 0;
-  }
-
-  .container ul li.el-menu-item {
-    font-size: 100%;
-  }
-
-  .container .mar-l {
-    padding: 0;
-  }
-
-  .container .content-wrapper {
-    padding: 20px;
-  }
 
 </style>
 
